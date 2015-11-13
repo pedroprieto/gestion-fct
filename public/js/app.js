5c14afc4debf61586b1c0023a3b2d7bfb366bce0
Pedro Prieto <pedpral@gmail.com>
2015-11-13 20:12:17 +0100

    Carga de librerías por https.

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,9 @@ requirejs.config({
     "baseUrl": "js/lib",
     "paths": {
 	"app": "../app",
-	"jquery": "http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min",
+	"jquery": "https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min",
 	'hammerjs': 'https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.4/hammer.min',
-	"materialize": "http://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min",
+	"materialize": "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min",
 	"jquery-hammer": "jquery.hammer"
     },
     shim: {
