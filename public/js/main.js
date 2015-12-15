ee0f751c6bbab5cbd572a668dde876ea48bd79ae
Pedro Prieto <pedpral@gmail.com>
2015-12-15 19:11:43 +0100

    El render inicial se hace con la url obtenida en lugar de /api.

--- public/js/main.js
+++ public/js/main.js
@@ -36,48 +36,47 @@ require([ 'ractive', 'rvc!ui/app' ], function ( Ractive, mainpage ) {
     'use strict';
 
     //ractiveload().then(function(c) {
-	console.log('Iniciando');
-	//console.log(Ractive.components);
-	Ractive.components['c1'] = mainpage;
+    console.log('Iniciando');
+    //console.log(Ractive.components);
+    Ractive.components['c1'] = mainpage;
 
-	var ractive = new Ractive({
-	    el: '#app',
-	    template: '<c1/>',
-	    enhance: true, //PROBAR
-	    data: function() {
-		// Cambiar a URL actual
-		req('/api/', 'get', null, this);
-		return {collection: {}};
-	    }
-	});
-
-
-	function req(url, method, body, ins) {
-	    // Disable submit buttons
-	    /*var x = document.querySelectorAll("[type=submit]");
-	      for (var i = 0; i < x.length; i++) {
-	      x[i].disabled = true;
-	      }*/
-	    var ajax = new XMLHttpRequest();
-	    ajax.onreadystatechange = function() {rsp(ajax,ins)};
-	    ajax.open(method, url);
-	    var ctype = "application/vnd.collection+json";
-	    ajax.setRequestHeader("accept",ctype);
-
-	    if(body && body!==null) {
-		ajax.setRequestHeader("content-type", ctype);
-	    }
-	    ajax.send(body);
-	    return false;
-	    
+    var ractive = new Ractive({
+	el: '#app',
+	template: '<c1/>',
+	enhance: true, //PROBAR
+	data: function() {
+	    // Cambiar a URL actual
+	    req(window.location.href , 'get', null, this);
+	    return {collection: {}};
 	}
+    });
 
-	function rsp(ajax, ins) {
 
-	    if(ajax.readyState===4) {
-		ins.set({'collection': JSON.parse(ajax.responseText).collection});
-	    }
+    function req(url, method, body, ins) {
+	// Disable submit buttons
+	/*var x = document.querySelectorAll("[type=submit]");
+	  for (var i = 0; i < x.length; i++) {
+	  x[i].disabled = true;
+	  }*/
+	var ajax = new XMLHttpRequest();
+	ajax.onreadystatechange = function() {rsp(ajax,ins)};
+	ajax.open(method, url);
+	var ctype = "application/vnd.collection+json";
+	ajax.setRequestHeader("accept",ctype);
+
+	if(body && body!==null) {
+	    ajax.setRequestHeader("content-type", ctype);
 	}
+	ajax.send(body);
+	return false;
+	
+    }
+
+    function rsp(ajax, ins) {
+	if(ajax.readyState===4) {
+	    ins.set({'collection': JSON.parse(ajax.responseText).collection});
+	}
+    }
 
     //});
 
