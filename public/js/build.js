cce525cb8dca9c45f6d465c411dff0c11b892d7b
Pedro Prieto <pedpral@gmail.com>
2016-02-02 00:56:58 +0100

    Corregido error en require.

--- public/js/build.js
+++ public/js/build.js
@@ -2,20 +2,9 @@
     "baseUrl": "./lib",
     "paths": {
 	"app": "..",
-	"jquery": "jquery-1.11.3.min",
 	"ractive": "ractive.min",
-	"ractive.load": "ractive-load.min",
 	"rvc": "rvc",
-	"ui": "../components",
-	'hammerjs': 'hammer-2.0.4.min',
-	'datepicker': 'picker',
-	'datepickerdate': 'picker.date',
-	'datepickertime': 'picker.time',
-	'datepicker_es': 'pickadate_es_ES',
-	"materialize": "materialize-0.97.3.min",
-	"validate": "validate.min",
-	"fetch": "fetch",
-	"jquery-hammer": "jquery.hammer"
+	"ui": "../components"
     },
     stubModules: [ 'rvc' ],
     name: "../main",
