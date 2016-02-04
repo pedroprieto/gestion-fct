35a924f5c06b28caff927f41a38bda0dadc153fe
Pedro Prieto <pedpral@gmail.com>
2016-02-04 16:55:59 +0100

    Cambio punto de entrada a la aplicación para cargar cliente nuevo.

--- resources/root.js
+++ resources/root.js
@@ -1,7 +1,8 @@
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-	res.redirect(302, req.routeToPath('cliente'));
+	//res.redirect(302, req.routeToPath('cliente'));
+	res.redirect(302, req.routeToPath('fcts', {user: req.user.username}));
     });
 
 }
