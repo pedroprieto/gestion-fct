67a757a99a22f29452db17032e331f5a777a8eb6
Pedro Prieto <pedpral@gmail.com>
2016-02-08 09:17:15 +0100

    Corregido error en error middleware.

--- errors/error_middleware.js
+++ errors/error_middleware.js
@@ -40,7 +40,7 @@ module.exports.respondError = function(err, req, res, next){
     var errcol = req.app.locals.errcj();
     errcol.href = req.protocol + '://' + req.get('host') + req.originalUrl;
     errcol.links = [];
-    errcol.links.push({href: errcol.href, prompt: "Volver", rel: "template"});
+    errcol.links.push(req.buildLink('fcts'));
     
     status = err.status || 500
     res.status(status)
