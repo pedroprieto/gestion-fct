3d7a0662e9690599b80b3dff904f9008ebc6af85
Pedro Prieto <pedpral@gmail.com>
2016-05-10 18:31:57 +0200

    Corregido script para letsencrypt. Pregunta ruta y texto para display.

--- letsencrypt.js
+++ letsencrypt.js
@@ -1,17 +1,23 @@
 
 var express = require('express');
 var app = express();
+var prompt = require('prompt');
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 app.set('protocol', process.env.NODE_ENV!='test'? 'https' : 'http');
 
-app.get('/.well-known/acme-challenge/', function (req, res) {
-  res.send('CLAVE LETSENCRYPT');
-});
+prompt.start();
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
-var server = app.listen(app.get('port'), ipaddress, function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+prompt.get(['ruta', 'clave'], function (err, result) {
 
+  app.get(result.ruta, function (req, res) {
+    res.send(result.clave);
+  });
+
+  var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
+  var server = app.listen(app.get('port'), ipaddress, function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+
+});
