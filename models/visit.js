7d078210d23ee79d3af38194c75ba205354b959e
Pedro Prieto <pedpral@gmail.com>
2019-01-14 18:08:43 +0100

    Corrijo fechas FM34

--- models/visit.js
+++ models/visit.js
@@ -247,7 +247,7 @@ visitSchema.statics.genfm34_cj = function (fm34) {
     item.data = [];
     item.links = [];
 
-    var isoweek = moment().isoWeek(fm34._id.semana).isoWeekYear(fm34._id.anyo);
+  var isoweek = moment().isoWeekYear(fm34._id.anyo).isoWeek(fm34._id.semana);
 
     var princ = isoweek.startOf('isoWeek').format("DD/MM/YYYY");
     var fin = isoweek.endOf('isoWeek').format("DD/MM/YYYY");
